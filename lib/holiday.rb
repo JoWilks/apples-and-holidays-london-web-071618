20ecb2b7c62512d05b4187427a6b051d30011cd6
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-04 14:16:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each { |season, holiday| 
     puts "#{season.capitalize}:"
       holiday.each { |name, supplies| 
-          if mod_name=name.replace("_"," ") 
+          if name.contains
+            mod_name=name.replace("_"," ") 
         puts "  #{name.humanise}: #{supplies.join(", ")}"
     }
   }
